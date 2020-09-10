84c0cf638ba0c397b118aa604e9d646875723d32
rish0229 <sushantsareen456@gmail.com>
2020-09-10 10:14:20 +0530

    incomplete

--- block-BJaadp/code/index.js
+++ block-BJaadp/code/index.js
@@ -16,18 +16,64 @@ let persons = [
 // NOTE: You can only use reduce method to solve this exercise:
 
 // Find the average grade
+console.log(persons.reduce((acc, elm) => acc + elm.grade / persons.length, 0));
 
 // Find the average grade of male
+let malePersons = persons.filter((elm) => elm.sex === 'M');
+console.log(
+  malePersons.reduce((acc, elm) => acc + elm.grade / malePersons.length, 0)
+);
 
 // Find the average grade of female
+let femalePersons = persons.filter((elm) => elm.sex === 'F');
+console.log(
+  femalePersons.reduce((acc, elm) => acc + elm.grade / femalePersons.length, 0)
+);
 
 // Find the highest grade
+console.log(
+  persons.reduce((acc, elm) => {
+    if (elm.grade > acc) {
+      acc = elm.grade;
+    }
+    return acc;
+  }, 1)
+);
 
 // Find the highest grade in male
+console.log(
+  malePersons.reduce((acc, elm) => {
+    if (elm.grade > acc) {
+      acc = elm.grade;
+    }
+    return acc;
+  }, 0)
+);
 
 // Find the highest grade in female
+console.log(
+  femalePersons.reduce((acc, elm) => {
+    if (elm.grade > acc) {
+      acc = elm.grade;
+    }
+    return acc;
+  }, 0)
+);
 
 // Find the highest grade for people whose name starts with 'J' or 'P'
+let nameJorP = persons.filter((elm) => {
+  if (elm.name[0] === 'J' || elm.name[0] === 'P') {
+    return true;
+  }
+});
+console.log(
+  nameJorP.reduce((acc, elm) => {
+    if (elm.grade > acc) {
+      acc = elm.grade;
+    }
+    return acc;
+  }, 0)
+);
 
 const fruitBasket = [
   'banana',
@@ -44,7 +90,6 @@ const fruitBasket = [
 ];
 
 /* 
-
 Use the fruitBasket array to create an object where key will be the fruit and value will be the number of times
 that fruit has appeared in the array.
 
@@ -52,15 +97,27 @@ Output:
 {banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1}
 */
 
-/* 
+let obj = fruitBasket.reduce((acc, elm) => {
+  if (acc[elm]) {
+    acc[elm] += 1;
+  } else {
+    acc[elm] = 1;
+  }
+  return acc;
+}, {});
+console.log(obj);
 
-Use the fruitBasket array to create an array of array. Each array will contain two values name of fruit and number of times
-that fruit appeared.
+/* 
+Use the fruitBasket array to create an array of array. Each array will contain two values name of fruit and number of times that fruit appeared.
 
 Output: 
-
 [['banana', 2], ['cherry', 3], ['orange', 3], ['apple', 2], ['fig', 1]]
 */
+console.log(
+  fruitBasket.reduce((acc, elm) => {
+    return acc;
+  }, [])
+);
 
 const data = [
   [1, 2, 3],
@@ -70,6 +127,14 @@ const data = [
 ];
 
 // Using reduce flat data array
+console.log(
+  data.reduce((acc, elm) => {
+    for (let char of elm) {
+      acc.push(char);
+    }
+    return acc;
+  }, [])
+);
 
 const dataTwo = [
   [1, 2, 3],
@@ -79,29 +144,39 @@ const dataTwo = [
 ];
 
 // Using reduce flat dataTwo array
+console.log(
+  dataTwo.reduce((acc, elm) => {
+    for (let char of elm) {
+      if (typeof char == 'object') {
+        for (let char2 of char) {
+          acc.push(char2);
+        }
+      } else {
+        acc.push(char);
+      }
+    }
+    return acc;
+  }, [])
+);
 
 /*
-
 Create these functions which accepts a number value and returns a number value:
   - `increment` adds one to the input value
   - `double` doubles the input value
   - `decrement` decrement 1 from the value 
   - `triple` triples the input 
   - `half` converts the value to half and return the integer value not decimal (use Math.round(21.5) => 21)
-
-
-
 */
 
 let pipeline = [
-  increment,
-  double,
-  decrement,
-  decrement,
-  double,
-  triple,
-  half,
-  increment,
+  'increment',
+  'double',
+  'decrement',
+  'decrement',
+  'double',
+  'triple',
+  'half',
+  'increment',
 ];
 
 /*
@@ -114,22 +189,21 @@ EXAMPLE:
   increment(3) - return 4
   double(4) - return 8
   decrement(8) - return 7
-
   ...
 */
 
 let pipeline2 = [
-  increment,
-  half,
-  double,
-  decrement,
-  decrement,
-  triple,
-  double,
-  triple,
-  half,
-  increment,
-  triple,
+  'increment',
+  'half',
+  'double',
+  'decrement',
+  'decrement',
+  'triple',
+  'double',
+  'triple',
+  'half',
+  'increment',
+  'triple',
 ];
 
 // Find the output using pipeline2 the initial value if 8
